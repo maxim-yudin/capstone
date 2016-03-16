b371a89dc581c17d6714f2e82881c7d095c51d02
Maxim Yudin <maxim.yudin@gmail.com>
2016-03-16 08:42:53 +0300

    [feat] VK sdk initialization

---
+++ VKFlow/app/src/main/java/jqsoft/apps/vk/flow/LoginActivity.java
@@ -0,0 +1,103 @@
+package jqsoft.apps.vk.flow;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.VKSdk.LoginState;
+import com.vk.sdk.api.VKError;
+
+/**
+ * Activity which displays a login screen to the user, offering registration as
+ * well.
+ */
+public class LoginActivity extends AppCompatActivity {
+    /**
+     * Scope is set of required permissions for your application
+     *
+     * @see <a href="https://vk.com/dev/permissions">vk.com api permissions documentation</a>
+     */
+    private static final String[] scope = new String[]{
+            VKScope.WALL,
+    };
+    private boolean isResumed = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VKSdk.wakeUpSession(this, new VKCallback<LoginState>() {
+            @Override
+            public void onResult(VKSdk.LoginState res) {
+                if (isResumed) {
+                    switch (res) {
+                        case LoggedOut:
+                            showLoginForm();
+                            break;
+                        case LoggedIn:
+                            showLogoutForm();
+                            break;
+                        case Pending:
+                            break;
+                        case Unknown:
+                            break;
+                    }
+                }
+            }
+
+            @Override
+            public void onError(VKError error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isResumed = true;
+        if (VKSdk.isLoggedIn()) {
+            showLogoutForm();
+        } else {
+            showLoginForm();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        isResumed = false;
+        super.onPause();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        VKCallback<VKAccessToken> callback = new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                // User passed Authorization
+            }
+
+            @Override
+            public void onError(VKError error) {
+                // User didn't pass Authorization
+            }
+        };
+
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, callback)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void showLogoutForm() {
+
+    }
+
+    private void showLoginForm() {
+
+    }
+}
---
+++ VKFlow/app/src/main/java/jqsoft/apps/vk/flow/VKFlowApplication.java
@@ -0,0 +1,33 @@
+package jqsoft.apps.vk.flow;
+
+import android.app.Application;
+import android.content.Intent;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKAccessTokenTracker;
+import com.vk.sdk.VKSdk;
+
+/**
+ * Created by maximyudin on 16.03.16.
+ */
+public class VKFlowApplication extends Application {
+    // Handling invalid access token
+    VKAccessTokenTracker vkAccessTokenTracker = new VKAccessTokenTracker() {
+        @Override
+        public void onVKAccessTokenChanged(VKAccessToken oldToken, VKAccessToken newToken) {
+            if (newToken == null) {
+                // if access token is invalidated, then we're running app with a clean slate
+                Intent intent = new Intent(VKFlowApplication.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        vkAccessTokenTracker.startTracking();
+        VKSdk.initialize(this);
+    }
+}
